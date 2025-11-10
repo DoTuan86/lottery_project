13ff68c11dd234b910f957a88f45d25b1b8dddaf
DoTuan86 <dongoctuan12101986@gmail.com>
2025-11-10 10:18:43 +0700

    day tat ca len git

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%nrz$hagvc1(j)-!@y77@dt5-9^+p)i(%x94#$llrrb1yu2dx9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
